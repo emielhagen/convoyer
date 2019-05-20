1187f4f45736575e73f885c55d4795e706b55ccc
emiel hagen <emiel.hagen@gmail.com>
2019-05-20 17:48:10 +1000

    views

--- app/controllers/convoys_controller.rb
+++ app/controllers/convoys_controller.rb
@@ -11,10 +11,13 @@ class ConvoysController < ApplicationController
   end
 
   def create
+    verify_locations(params[:from_location], params[:to_location])
+
     @convoy = Convoy.new(convoy_params)
     @convoy.save!
 
-    render action: 'index'
+    @convoys = Convoy.all
+    render action: 'show'
   end
 
   def destroy
@@ -24,6 +27,10 @@ class ConvoysController < ApplicationController
   end
 
   private
+
+  def verify_locations(from, to)
+    #TODO: first try to find locations in database, if not there, get it from MAP api
+  end
 
   def convoy_params
     params.require(:convoy).permit(:name, :start_date, :finish_date, :fwd_only, :user_id, :completed)
