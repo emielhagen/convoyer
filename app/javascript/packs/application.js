359335893d3efcec0b6c5f59e203620195a21cd7
emiel hagen <emiel.hagen@gmail.com>
2019-05-23 09:57:53 +1000

    sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,12 @@
 import "bootstrap";
 import typed from '../components/typed_components'
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "A nice alert",
+  text: "find me at app/javascript/packs/application.js",
+  icon: "success"
+});
 
 
 
